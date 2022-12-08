ec0cef6e548fe9833059cd5319381e41553a5e48
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-12-08 12:40:23 +0700

    added partial download

--- downloader.py
+++ downloader.py
@@ -9,11 +9,25 @@ FILE_PATH = Path(FILE_URL.path)
 
 OUTPUT_PATH = Path(__file__).parent
 
+SPLIT_NUM = 10
+
 def main():
-    s = requests.get(FILE_URL.geturl())
+    file_length = int(requests.head(FILE_URL.geturl()).headers['Content-Length'])
+    print(file_length)
+
+    chunk_size = file_length//SPLIT_NUM
+
+    content = b''
+    
+    for start in range(0, file_length, chunk_size):
+        headers = {'Range': f'bytes={start}-{start+chunk_size-1}'}
+
+        print(headers['Range'])
+        s = requests.get(FILE_URL.geturl(), headers=headers)
+        content += s.content
 
     with open(OUTPUT_PATH/FILE_PATH.name, 'wb') as f:
-        f.write(s.content)
+        f.write(content)
 
 if __name__ == '__main__':
     main()
