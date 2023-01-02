ca2c11c5af121972d2ca9d6a18370733883ef10c
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-01-02 20:01:42 +0700

    added true parallel download. refactored concurrent download

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
-import multiprocessing
+import aiomultiprocess
+import sys
 import asyncio
 import aiohttp
 from pathlib import Path
@@ -14,13 +15,26 @@ OUTPUT_PATH = Path(__file__).parent
 SPLIT_NUM = 10
 
 
-async def main():
-    content = await parallel_download(FILE_URL.geturl(), OUTPUT_PATH/FILE_PATH.name)
+async def main(*args):
+    await parallel_download(args)
 
 
-async def parallel_download(url, save_path):
-    file_length = int(requests.head(url).headers['Content-Length'])
-    chunk_size = file_length//SPLIT_NUM
+async def parallel_download(urls):
+    args = []
+    for url in urls:
+        file_url = urlparse(url)
+        args.append((file_url.geturl(), OUTPUT_PATH/Path(file_url.path).name))
+    async with aiomultiprocess.Pool() as pool:
+        await pool.starmap(concurrent_download, args)
+
+async def concurrent_download(url, save_path):
+    try:
+        file_length = int(requests.head(url).headers['Content-Length'])
+    except requests.exceptions.ConnectionError:
+        print(f'Connection to {url} refused')
+        return
+        
+    chunk_size = file_length//(SPLIT_NUM-1)
 
     content = b''
     downloads = []
@@ -32,17 +46,18 @@ async def parallel_download(url, save_path):
 
     with open(save_path, 'wb') as f:
         f.write(content)
+        print(f'({url}) saved download')
 
 
 async def partial_download(url, start_byte, chunk_size):
     headers = {'Range': f'bytes={start_byte}-{start_byte+chunk_size-1}'}
 
-    print(f'starting download {headers["Range"]}')
+    print(f'({url}) starting download {headers["Range"]}')
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as resp:
             content = await resp.read()
-            print(f'finished download {headers["Range"]}')
+            print(f'({url}) finished download {headers["Range"]}')
             return content
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(*sys.argv[1:]))
