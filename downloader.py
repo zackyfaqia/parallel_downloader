9244b9ce668e1b9000f58a9aa06890bc76d06d40
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-01-03 11:51:35 +0700

    added the use of temp file to free up some RAM

--- downloader.py
+++ downloader.py
@@ -2,6 +2,7 @@ import aiomultiprocess
 import sys
 import asyncio
 import aiohttp
+import tempfile
 from pathlib import Path
 from urllib.parse import urlparse
 
@@ -13,6 +14,7 @@ FILE_PATH = Path(FILE_URL.path)
 OUTPUT_PATH = Path(__file__).parent
 
 SPLIT_NUM = 10
+BUFFER_SIZE = 1024*5
 
 
 async def main(*args):
@@ -47,25 +49,36 @@ async def concurrent_download(url, save_path):
     content = b''
     downloads = []
 
-    for start in range(0, file_length, chunk_size):
-        downloads.append(partial_download(url, start, chunk_size))
+    for part_num, start in enumerate(range(0, file_length, chunk_size), 1):
+        downloads.append(partial_download(url, start, chunk_size, part_num))
     content = await asyncio.gather(*downloads)
-    content = b''.join(content)
+    # content = b''.join(content)
+    # print(content)
 
     with open(save_path, 'wb') as f:
-        f.write(content)
+        for part in content:
+            with open(part, 'rb') as p:
+                f.write(p.read())
+                # print(f'written {part}')
         print(f'({url}) saved download')
 
 
-async def partial_download(url, start_byte, chunk_size):
+async def partial_download(url, start_byte, chunk_size, part_num):
     headers = {'Range': f'bytes={start_byte}-{start_byte+chunk_size-1}'}
+    save_path = tempfile.gettempdir() + Path(urlparse(url).path).name + f'.part{part_num}'
 
     print(f'({url}) starting download {headers["Range"]}')
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as resp:
-            content = await resp.read()
+            # content = await resp.read()
             print(f'({url}) finished download {headers["Range"]}')
-            return content
+            with open(save_path, 'wb') as f:
+                # print('opened')
+                async for buffer in resp.content.iter_chunked(BUFFER_SIZE):
+                    # print(f'buffer: {buffer}')
+                    f.write(buffer)
+                # print('written')
+            return save_path
 
 if __name__ == '__main__':
     asyncio.run(main(*sys.argv[1:]))
